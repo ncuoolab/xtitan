d1504f2bb66f9e766055dbf21347f1164b7ad078
legnaleurc <legnaleurc@01c2abb6-1bc3-1142-8de2-3f33a7aa758b>
2012-02-12 11:28:28 +0000

    Synchronize.
    git-svn-id: http://oolab.csie.ncu.edu.tw/svn/oracleserver/trunk@13 01c2abb6-1bc3-1142-8de2-3f33a7aa758b

--- client/include/xTitanClient/xTitan.hpp
+++ client/include/xTitanClient/xTitan.hpp
@@ -3,11 +3,9 @@
 
 #include "xTitanClient/Spy/Spy.hpp"
 
-//#define xCheck(id,x)					QxSpy::getInstance()->encodeCheck(QString::fromUtf8(__FUNCTION__),id, x );
-//#define xCheck(x)						QxSpy::getInstance()->encodeCheck(QString::fromUtf8(__FUNCTION__),QString::fromUtf8(__FUNCTION__),x);
-#define xCheck(...)						QxSpy::getInstance()->encodeCheck(QString::fromUtf8(__FUNCTION__),__VA_ARGS__);
-#define xInput(...)						QxSpy::getInstance()->recordInput(this->objectName(),__FUNCTION__,__VA_ARGS__);
-#define xCustomInput(object,method,...)	QxSpy::getInstance()->recordInput(object,__FUNCTION__,method,__VA_ARGS__);
+#define xCheck(...)						xtitan::spy::Spy::getInstance().encodeCheck(QString::fromUtf8(__FUNCTION__),__VA_ARGS__);
+#define xInput(...)						xtitan::spy::Spy::getInstance().recordInput(this->objectName(),__FUNCTION__,__VA_ARGS__);
+#define xCustomInput(object,method,...)	xtitan::spy::Spy::getInstance().recordInput(object,__FUNCTION__,method,__VA_ARGS__);
 
 XTITAN_CLIENT_DLL void xRegisterObject( QObject * object, const QString & id = QString() );
 
