bf9160b18aefe88dcdaf6f00cb30c5a150513da5
Wei Cheng Pan <legnaleurc@gmail.com>
2013-07-22 18:02:00 +0800

    Add comment.

--- core/src/Network/SimpleSocket.cpp
+++ core/src/Network/SimpleSocket.cpp
@@ -9,9 +9,20 @@ Copyright (c) 2011 NCU OOlab <ncuoolab@gmail.com>
 #include "xTitan/Exception/NetworkError.hpp"
 
 
+/**
+ * @ingroup core
+ * @class xtitan::SimpleSocket
+ * @brief A simple network client
+ */
+
+
 using xtitan::SimpleSocket;
 
 
+/**
+ * @brief constructor
+ * @param [in] parent parent of this QObject
+ */
 SimpleSocket::SimpleSocket( QObject * parent ):
 QObject( parent ),
 p_( new Private( new QLocalSocket( this ), this ) ) {
@@ -32,54 +43,106 @@ p_( new Private( socket, this ) ) {
 //	socket->connect( socket, SIGNAL( disconnected() ), SLOT( deleteLater() ) );
 }
 
+/**
+ * @brief abort connection
+ * @sa close()
+ */
 void SimpleSocket::abort() {
 	this->p_->socket->abort();
 }
 
+/**
+ * @brief check if no pending data
+ * @return true if no pending data
+ */
 bool SimpleSocket::atEnd() const {
 	return this->p_->queue.isEmpty();
 }
 
+/**
+ * @brief close connection
+ * @sa abort()
+ */
 void SimpleSocket::close() {
 	this->p_->socket->close();
 }
 
+/**
+ * @brief connect to a server
+ * @param [in] name server name
+ * @param [in] openMode read or write
+ */
 void SimpleSocket::connectToServer( const QString & name, QIODevice::OpenMode openMode /* = QIODevice::ReadWrite */ ) {
 	this->p_->socket->connectToServer( name, openMode );
 }
 
+/**
+ * @brief disconnect from server
+ */
 void SimpleSocket::disconnectFromServer() {
 	this->p_->socket->disconnectFromServer();
 }
 
+/**
+ * @brief get last error code
+ * @return last error code
+ */
 QLocalSocket::LocalSocketError SimpleSocket::error() const {
 	return this->p_->socket->error();
 }
 
+/**
+ * @brief get last error message
+ * @return last error message
+ */
 QString SimpleSocket::errorString() const {
 	return this->p_->socket->errorString();
 }
 
+/**
+ * @brief flush pending socket
+ * @return true if data flushed
+ */
 bool SimpleSocket::flush() {
 	return this->p_->socket->flush();
 }
 
+/**
+ * @brief get full server name
+ * @return full server name
+ */
 QString SimpleSocket::fullServerName() const {
 	return this->p_->socket->fullServerName();
 }
 
+/**
+ * @brief check socket state
+ * @return true if socket is still connecting
+ */
 bool SimpleSocket::isValid() const {
 	return this->p_->socket->isValid();
 }
 
+/**
+ * @brief get short server name
+ * @return short server name
+ */
 QString SimpleSocket::serverName() const {
 	return this->p_->socket->serverName();
 }
 
+/**
+ * @brief get socket state
+ * @return current socket state
+ */
 QLocalSocket::LocalSocketState SimpleSocket::state() const {
 	return this->p_->socket->state();
 }
 
+/**
+ * @brief read one data
+ * @return a packet
+ */
 SimpleSocket::Packet SimpleSocket::read() {
 	if( this->p_->queue.isEmpty() ) {
 		throw NetworkError( "no message to read" );
@@ -87,6 +150,10 @@ SimpleSocket::Packet SimpleSocket::read() {
 	return this->p_->queue.dequeue();
 }
 
+/**
+ * @brief block until read something
+ * @return true if read something and not timeout
+ */
 bool SimpleSocket::waitForReadyRead( int msecs /* = 30000 */ ) {
 	QTimer * timer = new QTimer( this );
 	timer->setSingleShot( true );
@@ -102,6 +169,11 @@ bool SimpleSocket::waitForReadyRead( int msecs /* = 30000 */ ) {
 	return !this->atEnd();
 }
 
+/**
+ * @brief send packet
+ * @param [in] header packet header
+ * @param [in] data packet data
+ */
 void SimpleSocket::write( const QString & header, const QVariant & data ) const {
 	QByteArray block;
 	QDataStream dout( &block, QIODevice::WriteOnly );
